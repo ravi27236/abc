63da2b00ec93a51d635165ff3176baebc3986020
ravi27236 <oshadamd05@gmail.com>
2026-02-01 15:16:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=21dfde26fa3b888c",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=b2d7b1c2470a52a2",
 MONGODB: process.env.MONGODB || "mongodb+srv://qu6054:7XGfSRE66Yp7667k@cluster0.p7y66.mongodb.net/",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "+94727634194", 
 OWNER_NAME: process.env.OWNER_NAME || "Oshada",
